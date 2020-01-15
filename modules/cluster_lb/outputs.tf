078fde52419f531e0c9ecaaaf37c0fce4ae09171
rpieterick <rpieterick@users.noreply.github.com>
2020-01-14 18:31:40 -0600

    Update outputs.tf

--- modules/cluster_lb/outputs.tf
+++ modules/cluster_lb/outputs.tf
@@ -3,15 +3,15 @@ output "backend_pool_id" {
   description = "The id of the backend pool for this loadbalancer."
 }
 
-output "public_ip_address" {
-  value       = "${azurerm_public_ip.azlb.*.ip_address[0]}"
-  description = "The public ip address of this loadbalancer"
-}
+#output "public_ip_address" {
+#  value       = "${azurerm_public_ip.azlb.*.ip_address[0]}"
+#  description = "The public ip address of this loadbalancer"
+#}
 
-output "public_ip_id" {
-  value       = "${azurerm_public_ip.azlb.*.id[0]}"
-  description = "The id of the public ip resource in Azure of this loadbalancer"
-}
+#output "public_ip_id" {
+#  value       = "${azurerm_public_ip.azlb.*.id[0]}"
+#  description = "The id of the public ip resource in Azure of this loadbalancer"
+#}
 
 output "lb_endpoint_dns" {
 #  value       = "${azurerm_dns_a_record.api.name}.${azurerm_dns_a_record.api.zone_name}"
